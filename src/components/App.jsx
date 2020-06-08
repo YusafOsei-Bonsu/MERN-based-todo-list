b3ac4b86992319db11a7407ce0aa4d6d471dd262
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-08 14:01:50 +0100

    Route Configuration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import '../styles/App.css';
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,9 +8,16 @@ class App extends React.Component {
   
   render = () => {
     return (
-      <div className="container">
-        <h2>MERN Todo List</h2>
-      </div>
+      <Router>
+        <div className="container">
+          <h2>MERN Todo List</h2>
+        </div>
+        <Switch>
+          <Route exact path="/" component={ TodosList }></Route>
+          <Route path="/edit/:id" component={ EditTodo }></Route>
+          <Route path="/create" component={ CreateTodo }></Route>
+        </Switch>
+      </Router>
     );
   } 
  
