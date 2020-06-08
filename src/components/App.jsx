5f526969f92f8b42ea9d855631a2a11e2c77bb7b
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-06-08 13:49:50 +0100

    Project file structure & bootstrap (#2)
    * Transferred all .css files into the styles directory
    
    * Installed and imported bootstrap
    
    * Placed test file into a tests directory
    
    * Update index.js
    
    * Turned JS file into JSX
    
    * Changed App.jsx component

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/App.css';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends React.Component {
+  
+  render = () => {
+    return (
+      <div className="container">
+        <h2>MERN Todo List</h2>
+      </div>
+    );
+  } 
+ 
+}
+
+export default App;
