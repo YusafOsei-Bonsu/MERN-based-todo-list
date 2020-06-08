2a835a4ffbd315b079261bf32a4caaa83a03911c
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-08 15:45:50 +0100

    Component that creates to-do tasks

---
+++ src/components/CreateTodo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CreateTodo extends React.Component {
+    render = () => {
+        return (
+            <div>
+                <p>Welcome to Create Todo Component!!</p>
+            </div>
+        );
+    }
+}
+
+export default CreateTodo;
