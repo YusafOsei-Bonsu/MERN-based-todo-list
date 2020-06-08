104607569d91f21dfa543e0900658fbad0c65853
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-08 16:47:07 +0100

    Indenting App.jsx

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -14,22 +14,22 @@ class App extends React.Component {
     return (
       <Router>
         <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank" rel="noopener noreferrer"><Logo /></a>
-          <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
-          <div className="collpase navbar-collapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="navbar-item"><Link to="/" className="nav-link">Todos</Link></li>
-              <li className="navbar-item"><Link to="/create" className="nav-link">Create Todo</Link></li>
-            </ul>
-          </div>
-        </nav>
-        <br/>
-        <Switch>
-          <Route exact path="/" component={ TodoList } />
-          <Route path="/edit/:id" component={ EditTodo } />
-          <Route path="/create" component={ CreateTodo } />
-        </Switch>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank" rel="noopener noreferrer"><Logo /></a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item"><Link to="/" className="nav-link">Todos</Link></li>
+                <li className="navbar-item"><Link to="/create" className="nav-link">Create Todo</Link></li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Switch>
+            <Route exact path="/" component={ TodoList } />
+            <Route path="/edit/:id" component={ EditTodo } />
+            <Route path="/create" component={ CreateTodo } />
+          </Switch>
         </div>
       </Router>
     );
