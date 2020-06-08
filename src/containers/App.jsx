6410092dffabdd64e9b8a19dd17776e76fc36145
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-08 15:44:59 +0100

    App component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import CreateTodo from '../components/CreateTodo.jsx';
+import TodoList from '../components/TodoList.jsx';
+import EditTodo from '../components/EditTodo.jsx';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import '../styles/App.css';
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -13,7 +16,7 @@ class App extends React.Component {
           <h2>MERN Todo List</h2>
         </div>
         <Switch>
-          <Route exact path="/" component={ TodosList }></Route>
+          <Route exact path="/" component={ TodoList }></Route>
           <Route path="/edit/:id" component={ EditTodo }></Route>
           <Route path="/create" component={ CreateTodo }></Route>
         </Switch>
