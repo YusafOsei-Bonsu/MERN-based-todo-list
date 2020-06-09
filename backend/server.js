cd8ffdc9c72f6a60694e6708a7dd358c2e2202ba
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-09 08:50:15 +0100

    Connecting to the MongoDB db via Mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require('mongoose');
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/todos", { useNewUrlParser: true, useUnifiedTopology: true  });
+const connection = mongoose.connection;
+
+// Message is printed to confirm successful connection to the MongoDB db
+connection.once('open', () => console.log("MongoDB database connection established successfully"));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
