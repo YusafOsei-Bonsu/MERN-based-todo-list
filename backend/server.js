fdaf5aa83fd401a2dc8f354e01bed1b23b9291ab
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-09 10:11:28 +0100

    An endpoint that changes an existing todo item within the db

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,24 @@ todoRoutes.route('/add').post((req, res) => {
         .catch(err => res.status(400).send("Inserting new todo failed!!"));
 });
 
+// Changes an existing todo in the MongoDB db
+todoRoutes.route("/update/:id").post((req, res) => {
+    Todo.findById(req.params.id, (err, todo) => {
+        if (!todo) {
+            res.status(404).send("data is not found!");
+        } else {
+            todo.todo_description = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+
+            todo.save()
+                .then(todo => res.json("Todo updated!"))
+                .catch(err => res.status(400).send("Update not possible!"));
+        }
+    });
+});
+
 // Message is printed to confirm successful connection to the MongoDB db
 connection.once('open', () => console.log("MongoDB database connection established successfully"));
 
