0bbaa6027f2583f450530f7996572fa2da46a1a5
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-10 14:27:25 +0100

    .completed class
    Crosses through the task after pressing the "Completed" checkbox

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,10 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Todo = (props) => (
     <tr>
-        <td>{ props.todo.todo_description }</td>
-        <td>{ props.todo.todo_responsible }</td>
-        <td>{ props.todo.todo_priority }</td>
-        <td>{ props.todo.todo_priority }</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{ props.todo.todo_description }</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{ props.todo.todo_responsible }</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{ props.todo.todo_priority }</td>
         <td><Link to={"/edit/"+props.todo._id}>Edit</Link></td>
     </tr>
 );
