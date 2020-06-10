80a4eb6f23c26f649bc6499259ac5b6bfc5599f7
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-06-10 12:12:56 +0100

    Displaying each to-do task in a table

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import Axios from 'axios';
+import Todo from './Todo.jsx';
 
 class TodoList extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        }
+    }
+
+    // Display every to-do task
+    todoList = () => this.state.todos.map((currentTodo, i) => <Todo todo={currentTodo} key={i} />);
+
+    // Retrieving todos data from the MongoDB database
+    componentDidMount = () => {
+        Axios.get("http://localhost:4000/todos/")
+            .then(res => this.setState({ todos: res.data }))
+            .catch(err => console.log(err));
+    }
+
     render = () => {
         return (
             <div>
-                <p>Welcome to the Todo List Component!!</p>
+                <h3>Todos List</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }}>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Responsible</th>
+                            <th>Priority</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.TodoList() }
+                    </tbody>
+                </table>
             </div>
         );
     }
